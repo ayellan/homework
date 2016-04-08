e179ed63c1460d487c7a96bb97afb93d752a5308
ayellan <AllanTina@ALLANs-MacBook-Pro.local>
2016-04-07 18:46:09 -0700

    Wireframes Sync

--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -1,1 +1,25 @@
+//convert -32 /5.8
 
+$('#fahrenheit_to_celsius').on('click', function() {
+	var temperatureNum = $('#temperature').val();
+	var tempF = parseInt(temperatureNum, 10);
+	var tempC = parseInt(temperatureNum,10);
+	/*var tempFtoC = (temperatureNum - 32) * (5/9);*/
+	var tempFtoC = (temperatureNum - 32) / 1.8;
+	/*var tempCtoF = (temperatureNum * (9/5)) + 32;*/
+	var tempCtoF = 1.8 * temperatureNum + 32;
+
+$('#result').html(tempF + 'ºF' + ' = ' + tempFtoC + 'ºC');
+});
+
+$('#celsius_to_fahrenheit').on('click', function() {
+	var temperatureNum = $('#temperature').val();
+	var tempF = parseInt(temperatureNum, 10);
+	var tempC = parseInt(temperatureNum, 10);
+	/* var tempFtoC = (temperatureNum - 32) * (5/9);*/
+	var tempFtoC = (temperatureNum - 32) / 1.8;
+	/*var tempCtoF = (temperatureNum * (9/5)) + 32;*/
+	var tempCtoF = 1.8 * temperatureNum + 32;
+
+$('#result').html(tempC + 'ºC' +' = ' + tempCtoF + 'ºF');
+});
