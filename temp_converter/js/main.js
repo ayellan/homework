fd429b2e2bf5b3fe4599990212828f49900f59f4
ayellan <AllanTina@ALLANs-MacBook-Pro.local>
2016-04-11 00:08:04 -0700

    hw5 final commit

--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -1,25 +1,49 @@
-//convert -32 /5.8
-
+//when #fahrenheit_to_celsius is clicked.
 $('#fahrenheit_to_celsius').on('click', function() {
-	var temperatureNum = $('#temperature').val();
-	var tempF = parseInt(temperatureNum, 10);
-	var tempC = parseInt(temperatureNum,10);
-	/*var tempFtoC = (temperatureNum - 32) * (5/9);*/
+    var temperatureNum = $('#temperature').val();
+	var tempF = parseFloat(temperatureNum);
+	var tempC = parseFloat(temperatureNum);
 	var tempFtoC = (temperatureNum - 32) / 1.8;
-	/*var tempCtoF = (temperatureNum * (9/5)) + 32;*/
 	var tempCtoF = 1.8 * temperatureNum + 32;
-
+//change html in #result.
 $('#result').html(tempF + 'ºF' + ' = ' + tempFtoC + 'ºC');
+
+//If statement to prove first condition.
+if(tempF < 50) {
+	$('body').removeClass();
+	$('body').addClass('bluebg');
+//If else statement to prove a second condition.
+} else if (tempF >= 50 && tempF < 80) {
+	$('body').removeClass();
+	$('body').addClass('orangebg');
+//Else statement for not meeting any conditions.
+} else {
+	$('body').removeClass();
+	$('body').addClass('redbg');
+		}
 });
 
+//when #celsius_to_fahrenheit is clicked.
 $('#celsius_to_fahrenheit').on('click', function() {
-	var temperatureNum = $('#temperature').val();
-	var tempF = parseInt(temperatureNum, 10);
-	var tempC = parseInt(temperatureNum, 10);
-	/* var tempFtoC = (temperatureNum - 32) * (5/9);*/
+    var temperatureNum = $('#temperature').val();
+	var tempF = parseFloat(temperatureNum);
+	var tempC = parseFloat(temperatureNum);
 	var tempFtoC = (temperatureNum - 32) / 1.8;
-	/*var tempCtoF = (temperatureNum * (9/5)) + 32;*/
 	var tempCtoF = 1.8 * temperatureNum + 32;
-
+//change html in #result.
 $('#result').html(tempC + 'ºC' +' = ' + tempCtoF + 'ºF');
+
+//If statement to prove first condition.
+if(tempC < 15) {
+	$('body').removeClass();
+	$('body').addClass('bluebg');
+//If else statement to prove a second condition.	
+} else if (tempC >= 15 && tempC < 25) {
+	$('body').removeClass();
+	$('body').addClass('orangebg');
+//Else statement for not meeting any conditions.	
+} else {
+	$('body').removeClass();
+	$('body').addClass('redbg');
+		}
 });
